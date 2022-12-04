1d14cd16ff4c5078e5080c914db500058a8f719c
Jason Li <jasonli1337@gmail.com>
2022-12-04 16:46:54 -0500

    Add shared utility for parsing text

---
+++ Assets/SharedUtility/AdventUtilities.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace SharedUtility
+{
+    public static class AdventUtilities
+    {
+        /// <summary>
+        /// Take in the TextAsset and process it into a string array
+        /// </summary>
+        /// <param name="text">text asset with input data, \n delimited</param>
+        /// <returns>Array of all lines as strings</returns>
+        public static string[] ProcessTextAssetIntoStringArray(TextAsset text)
+        {
+            TextAsset inputAsset = text;
+
+            string[] allLines = inputAsset.text.Split('\n');
+
+            Debug.Log($"Lines: {allLines.Length}");
+
+            return allLines;
+        }
+    }
+}
