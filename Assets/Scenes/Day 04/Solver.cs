4079e88e891dd9da973bed59ece332e1433c6115
Jason Li <jasonli1337@gmail.com>
2022-12-04 16:48:45 -0500

    Start Day 04 puzzle

---
+++ Assets/Scenes/Day 04/Solver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SharedUtility;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Scenes.Day_04
+{
+    public class Solver : MonoBehaviour
+    {
+        [SerializeField] private AssetReference _addressableTextAsset = null;
+
+        private string[] _inputLines;
+
+        void Start()
+        {
+            _addressableTextAsset.LoadAssetAsync<TextAsset>().Completed += handle =>
+            {
+                Debug.Log($"Length: {handle.Result.text.Length}");
+
+                _inputLines = AdventUtilities.ProcessTextAssetIntoStringArray(handle.Result);
+
+                Process(_inputLines);
+            };
+
+        }
+
+        void Process(string[] inputs)
+        {
+           
+        }
+
+
+    }
+}
