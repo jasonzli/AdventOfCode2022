ce3fd4c881d5c56ac17adc1a66f71bcafd10d533
Jason Li <jasonli1337@gmail.com>
2022-12-05 15:58:30 -0500

    Solve Part 1
    Mostly the input handling and then the stack was easy

--- Assets/Scenes/Day 05/Solver.cs
+++ Assets/Scenes/Day 05/Solver.cs
@@ -40,15 +40,84 @@ namespace Scenes.Day_05
             // Need to process the grid strings into stacks, so we can push and pop from them, but they are delimited weirdly
             // so we have to manipulate them into a form that we can use.
             
-            // First, get the amount of columns in the grid from the last row in the 
-            // a string regex pattern that captures all numbers separated by spaces
-            string columnMatchPattern = @"(\d+)";
-            string lastRow = gridOfCrates.Last();
-            int columns = int.Parse(Regex.Match(lastRow, columnMatchPattern, RegexOptions.RightToLeft).Groups.Last().Value);
-
+            List<Stack<Crate>> stacks = new List<Stack<Crate>>();
             
+            // We have the empty line and know where the grid ends
+            string lastRow = gridOfCrates.Last(); // we can go through this and find the number of columns *and* the index of the crates too
+            int column = 1;
+            Dictionary<int, int> crateColumnIndex = new Dictionary<int, int>();
+            
+            while (lastRow.IndexOf(column.ToString()) != -1) // while we can find column in this last row
+            {
+                int columnIndex = lastRow.IndexOf(column.ToString()); //this is the index of the column's contents
 
+                Stack<Crate> crateStackAtIndex = new Stack<Crate>();
+                for (int i = splitIndex - 2; i >= 0; i--) //go through rows backwards
+                {
+                    char crateContent = gridOfCrates[i][columnIndex];
+                    if (crateContent == ' ')
+                    {
+                        break;
+                    }
+                        
+                    crateStackAtIndex.Push(new Crate(crateContent));
+                    
+                }
+
+                stacks.Add(crateStackAtIndex);
+                
+                crateColumnIndex.Add(column, columnIndex); 
+                column++; //increment forward
+            }
+            
+            //Stacks now has all the crates in order, we just have to process the movements of them as commands
+            string movementRegexPattern = @"move (\d+) from (\d+) to (\d+)";
+            foreach (string movement in movements)
+            {
+                //match the pattern against the movement string
+                Match match = Regex.Match(movement, movementRegexPattern);
+                if (match.Success)
+                {
+                    int numberOfCrates = int.Parse(match.Groups[1].Value);
+                    int fromColumn = int.Parse(match.Groups[2].Value);
+                    int toColumn = int.Parse(match.Groups[3].Value);
+                    
+                    MoveCratesBetweenStacks(numberOfCrates, stacks[fromColumn-1], stacks[toColumn-1]);
+                }
+            }
+            
+            // Read the first entry in all stacks as one string
+            string finalString = "";
+            foreach (Stack<Crate> stack in stacks)
+            {
+                finalString += stack.Peek().CrateContent;
+            }
+            
+            Debug.Log($"Final crates are: {finalString}");
         }
-        
+
+
+        private class Crate
+        {
+            public char CrateContent { get; private set; }
+            public Crate( char crateContent)
+            {
+                CrateContent = crateContent;
+            }
+        }
+
+        private void MoveCratesBetweenStacks(int quantity, Stack<Crate> origin, Stack<Crate> target)
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                Crate temp = origin.Pop();
+                target.Push(temp);
+            }
+        }
+
+        private void MoveCratesBetweenStacksWithQueue(int quantity, Stack<Crate> origin, Stack<Crate> target)
+        {
+            
+        }
     }
 }
