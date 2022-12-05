7448f9cebc168a42e3828e00b1cf450c06bcec07
Jason Li <jasonli1337@gmail.com>
2022-12-05 00:33:23 -0500

    Begin Puzzle 5
    Weird input

---
+++ Assets/Scenes/Day 05/Solver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using SharedUtility;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Scenes.Day_05
+{
+    public class Solver : MonoBehaviour
+    {
+        [SerializeField] private AssetReference _addressableTextAsset = null;
+
+        private string[] _inputLines;
+
+        void Start()
+        {
+            _addressableTextAsset.LoadAssetAsync<TextAsset>().Completed += handle =>
+            {
+                Debug.Log($"Length: {handle.Result.text.Length}");
+
+                _inputLines = AdventUtilities.ProcessTextAssetIntoStringArray(handle.Result);
+
+                Process(_inputLines);
+            };
+
+        }
+
+        void Process(string[] inputs)
+        {
+            
+
+        }
+        
+    }
+}
