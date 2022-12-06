0fef6b07800afe9c9825e72edc218dd7e5f44998
Jason Li <jasonli1337@gmail.com>
2022-12-06 02:55:08 -0500

    Solve problem 1

---
+++ Assets/Scenes/Day 06/Solver.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SharedUtility;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Scenes.Day_06
+{
+    public class Solver : MonoBehaviour
+    {
+        [SerializeField] private AssetReference _addressableTextAsset = null;
+
+        private string[] _inputLines;
+
+        void Start()
+        {
+            _addressableTextAsset.LoadAssetAsync<TextAsset>().Completed += handle =>
+            {
+                Debug.Log($"Length: {handle.Result.text.Length}");
+
+                _inputLines = AdventUtilities.ProcessTextAssetIntoStringArray(handle.Result);
+
+                Process(_inputLines);
+            };
+
+        }
+
+        void Process(string[] inputs)
+        {
+           // Read each line of the inputs and find the index of the first instance of 4 unique characters
+           int index = -1;
+           foreach (string input in inputs)
+           {
+               index = IndexOfFirstFourUniqueCharactersInString(input,4);
+           }
+           
+           Debug.Log($"The index of the unique string is {index}");
+        }
+
+        int IndexOfFirstFourUniqueCharactersInString(string input, int length)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                string subString = input.Substring(i, length); 
+                if (subString.Length == length && subString.Distinct().Count() == length)
+                {
+                    return i + length;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
