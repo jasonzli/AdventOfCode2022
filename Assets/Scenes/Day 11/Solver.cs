81fec908bca1f7ff921591b5d5844f37b210b985
Jason <jasonli1337@gmail.com>
2022-12-13 00:09:39 -0500

    Add puzzle 11

---
+++ Assets/Scenes/Day 11/Solver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SharedUtility;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime;
+using Unity.VisualScripting.ReorderableList;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Scenes.Day_11
+{
+    public class Solver : MonoBehaviour
+    {
+        [SerializeField] private AssetReference _addressableTextAsset = null;
+
+        private string[] _inputLines;
+
+        void Start()
+        {
+            _addressableTextAsset.LoadAssetAsync<TextAsset>().Completed += handle =>
+            {
+                _inputLines = AdventUtilities.ProcessTextAssetIntoStringArray(handle.Result);
+                Process(_inputLines);
+            };
+
+        }
+
+        void Process(string[] inputs)
+        {
+            
+        }
+
+       
+        
+    }
+}
